53b1de97c1bc2b298ba3ffe425c2e182e1e838eb
Guigw <guigw+admin@gpsnet.fr>
2021-11-14 12:17:11 +0100

    add tests

--- src/Listener/AbstractControllerSubscriber.php
+++ src/Listener/AbstractControllerSubscriber.php
@@ -3,7 +3,6 @@
 namespace Yrial\Simrandom\Listener;
 
 use ReflectionClass;
-use ReflectionException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ControllerEvent;
 
@@ -13,16 +12,12 @@ abstract class AbstractControllerSubscriber implements EventSubscriberInterface
     {
         $controller = $event->getController();
         if (is_array($controller)) {
-            try {
-                $reflectionClass = new ReflectionClass($controller[0]);
-                $method = $reflectionClass->getMethod($controller[1]);
-                //Vérification de la conf
-                $attributes = $method?->getAttributes($class);
-                if (is_iterable($attributes)) {
-                    return $attributes[0]->newInstance();
-                }
-            } catch (ReflectionException $e) {
-                //Si jamais exception alors rien
+            $reflectionClass = new ReflectionClass($controller[0]);
+            $method = $reflectionClass->getMethod($controller[1]);
+            //Vérification de la conf
+            $attributes = $method?->getAttributes($class);
+            if (is_iterable($attributes)) {
+                return $attributes[0]->newInstance();
             }
         }
         return null;
